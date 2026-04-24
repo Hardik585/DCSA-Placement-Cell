e70bca4aff685ef65d916b8d2a638bef3ce55780
Hardik585 <Hardikkaushik83126@gmail.com>
2026-04-24 19:41:44 +0530

    fix the role bug issue

--- backend/src/main/java/com/hardik/backend/controller/AuthController.java
+++ backend/src/main/java/com/hardik/backend/controller/AuthController.java
@@ -1,12 +1,15 @@
 package com.hardik.backend.controller;
 
 import com.hardik.backend.dto.LoginRequest;
-import com.hardik.backend.model.UserEntity;
-import com.hardik.backend.repository.UserRepository;
+import com.hardik.backend.service.AppUserDetailsService;
 import com.hardik.backend.utils.JwtUtil;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,20 +22,31 @@ import java.util.Map;
 @RequiredArgsConstructor
 public class AuthController {
 
-    private final UserRepository userRepository;
+
     private final JwtUtil jwtUtil;
+    private final AuthenticationManager authenticationManager;
+    private final AppUserDetailsService appUserDetailsService;
 
     @PostMapping("/login")
     public ResponseEntity<?> login(@Valid @RequestBody LoginRequest request) {
-        UserEntity user = userRepository.findByEmail(request.getEmail())
-                .orElseThrow(() -> new RuntimeException("User not found"));
-        if (!user.getPassword().equals(request.getPassword())) {
-            throw new RuntimeException("Invalid credentials");
-        }
-        String token = jwtUtil.generateToken(user.getEmail(), user.getRole().name());
+        Authentication authenticate = authenticate(request.getEmail(), request.getPassword());
+        UserDetails userDetails = (UserDetails) authenticate.getPrincipal();
 
+        //extract role from authentication
+        String role = userDetails
+                .getAuthorities()
+                .iterator()
+                .next()
+                .getAuthority()
+                .replace("ROLE_", "");
+
+        String token = jwtUtil.generateToken(userDetails.getUsername(), role);
         return ResponseEntity.ok(Map.of(
                 "token", token
         ));
+    }
+
+    private Authentication authenticate(String email, String password) {
+        return authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(email, password));
     }
 }
